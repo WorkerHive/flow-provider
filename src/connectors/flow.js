b6bfb09ea95f1028699b0424aad86b6e7b2ec111
Ross Leitch <professional.balbatross@gmail.com>
2020-12-31 16:40:29 +1300

    MergedAdapter fix?

--- src/connectors/flow.js
+++ src/connectors/flow.js
@@ -11,53 +11,49 @@ class FlowConnector{
     }
 
     async add(type, object){
-        let flowDef = this.flowDefs[type];
-                            
+        let flowDef = this.flowDefs[type] || {};          
         let path = new FlowPath(this.typeMap[type], flowDef)
-
         let batches = path.getBatched();
-
-        let adapter = new MergedAdapter(this.typeMap[type], this.stores, batches)
+        let adapter = MergedAdapter(this.typeMap[type], this.stores, batches)
         let result =  await adapter.addProvider()(object)                            
         return result;
     }
 
     async put(type, id, update){
-        let flowDef = this.flowDefs[type];
+        let flowDef = this.flowDefs[type] || {};
         let path = new FlowPath(this.typeMap[type], flowDef)
         let batches = path.getBatched();
-        let adapter = new MergedAdapter(this.typeMap[type], this.stores, batches)
+        let adapter = MergedAdapter(this.typeMap[type], this.stores, batches)
         let result = await adapter.putProvider()(id, update)
         return result;
 
     }
 
     async delete(type, id){
-        let flowDef = this.flowDefs[type];
+        let flowDef = this.flowDefs[type] || {};
         let path = new FlowPath(this.typeMap[type], flowDef)
         let batches = path.getBatched();
-        let adapter = new MergedAdapter(this.typeMap[type], this.stores, batches)
+        let adapter = MergedAdapter(this.typeMap[type], this.stores, batches)
         let result = await adapter.deleteProvider()(id)
         return result;
     }
 
     async get(type, query){
-        console.log("GET", this.flowDefs[type])
         let flowDef = this.flowDefs[type] || {};
         let path = new FlowPath(this.typeMap[type], flowDef)
         let batches = path.getBatched();
         console.log("Get with", type, query)
-        let adapter = new MergedAdapter(this.typeMap[type], this.stores, batches)
+        let adapter = MergedAdapter(this.typeMap[type], this.stores, batches)
         let result = await adapter.getProvider()(query)
         console.log("RESULT", result)
         return result;
     }
 
     async getAll(type){
-        let flowDef = this.flowDefs[type];
+        let flowDef = this.flowDefs[type] || {};
         let path = new FlowPath(this.typeMap[type], flowDef)
         let batches = path.getBatched();
-        let adapter = new MergedAdapter(this.typeMap[type], this.stores, batches)
+        let adapter = MergedAdapter(this.typeMap[type], this.stores, batches)
         let result = await adapter.getAllProvider()()
         return result;
     }
