d9f7d6c6403d6c4e07701d884772e60cba6d43e4
Ross Leitch <professional.balbatross@gmail.com>
2021-01-03 13:04:33 +1300

    update for more logging

--- src/transforms/input.js
+++ src/transforms/input.js
@@ -17,7 +17,7 @@ function inputTransform (){
                     newType = GraphQLInt;
                     break;
             }
-            console.log(type.fields[i])
+            console.log(type.fields[i].astNode.type)
             fields[type.fields[i].name] = {type: type.fields[i].type}
         }
         return new GraphQLInputObjectType({
