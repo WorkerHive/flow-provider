0d11db89e71e3dd7f4d94b12429b900635c6b0a7
Ross Leitch <professional.balbatross@gmail.com>
2021-01-18 11:37:56 +1300

    Trying to track down the readAll bug

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -8,8 +8,8 @@ const { objectFlip } = require('../utils/flow-query');
 import BaseAdapter from './base-adapter'
 const { merge, isEqual, unionWith } = require('lodash')
 
-export default class MergedAdapter extends BaseAdapter{
-    constructor(type : ObjectTypeComposer<any>, storeList, paths){
+export default class MergedAdapter extends BaseAdapter {
+    constructor(type: ObjectTypeComposer<any>, storeList, paths) {
         super();
         this.type = type.getType();
         this.storeList = storeList;
@@ -17,7 +17,7 @@ export default class MergedAdapter extends BaseAdapter{
     }
 
 
-    sortActions(type){
+    sortActions(type) {
         let refs = this.paths.refs || {};
         let paths = Object.assign({}, this.paths)
         delete paths.refs;
@@ -25,8 +25,8 @@ export default class MergedAdapter extends BaseAdapter{
         let primaryActions = [];
         let supportingActions = [];
 
-        for(var store in paths){
-            for(var path in paths[store]){
+        for (var store in paths) {
+            for (var path in paths[store]) {
                 let map = objectFlip(Object.assign({}, paths[store][path]))
 
 
@@ -34,40 +34,40 @@ export default class MergedAdapter extends BaseAdapter{
                     return Object.keys(map).indexOf(x)
                 }).filter((a) => a > -1).length > 0
 
-                if(refKey){
+                if (refKey) {
                     primaryActions.push({
                         [store]: {
                             [path]: paths[store][path]
                         }
                     })
-                }else{
+                } else {
                     supportingActions.push({
                         [store]: {
                             [path]: paths[store][path]
                         }
                     })
                 }
-                
+
             }
         }
 
-        return {primaryActions, supportingActions}
-        
+        return { primaryActions, supportingActions }
+
     }
 
-    iteratePaths(arr, iteree){
+    iteratePaths(arr, iteree) {
         arr.forEach(action => {
-            for(var store in action){
-                for(var path in action[store]){
+            for (var store in action) {
+                for (var path in action[store]) {
                     iteree(store, path, action)
                 }
             }
         })
     }
 
-    doActions(get_func){
+    doActions(get_func) {
         const { primaryActions, supportingActions } = this.sortActions(this.type);
-        const refs  = this.paths.refs || {};
+        const refs = this.paths.refs || {};
 
         let actions = [];
         let supporting = [];
@@ -84,9 +84,9 @@ export default class MergedAdapter extends BaseAdapter{
             let adapter = this.storeList.get(store).getAdapter();
 
             let _refs = {};
-            for(var k in refs){
-                let indx = refs[k].map((x) => x.split(':').slice(0,2).join(':')).indexOf(`${store}:${path}`)
-                if(indx > -1){
+            for (var k in refs) {
+                let indx = refs[k].map((x) => x.split(':').slice(0, 2).join(':')).indexOf(`${store}:${path}`)
+                if (indx > -1) {
                     _refs[refs[k][indx].split(':')[2]] = k
                 }
             }
@@ -96,14 +96,14 @@ export default class MergedAdapter extends BaseAdapter{
             let func = get_func(adapter, path, _refs)
             supporting.push(func)
         })
-    
-        return {actions, supporting}
+
+        return { actions, supporting }
     }
 
-    getProvider(){
+    getProvider() {
 
         const { actions, supporting } = this.doActions((adapter, bucket, provides) => {
-            return adapter.getProvider({name: bucket}, this.type, provides)
+            return adapter.getProvider({ name: bucket }, this.type, provides)
         })
 
         console.log(actions.length, supporting.length)
@@ -114,14 +114,14 @@ export default class MergedAdapter extends BaseAdapter{
                 results.forEach(item => {
                     merge(r, item)
                 })
-                if(supporting.length > 0){
+                if (supporting.length > 0) {
                     return Promise.all(supporting.map((action) => action(query))).then((endResult) => {
-                            endResult.forEach(it => {
+                        endResult.forEach(it => {
                             merge(r, it)
                         })
                         return r;
                     })
-                }else{
+                } else {
                     return r;
                 }
 
@@ -129,11 +129,11 @@ export default class MergedAdapter extends BaseAdapter{
         }
     }
 
-    getAllProvider(){
+    getAllProvider() {
         const { refs } = this.paths;
 
         const { actions, supporting } = this.doActions((adapter, bucket, provides) => {
-            return adapter.getAllProvider({name: bucket}, this.type, provides)
+            return adapter.getAllProvider({ name: bucket }, this.type, provides)
         })
 
         return () => {
@@ -149,18 +149,17 @@ export default class MergedAdapter extends BaseAdapter{
             */
 
             return Promise.all(actions.map((x) => x())).then((result) => {
-                let r  = result;
+                let r = result;
+
 
                 return Promise.all(supporting.map((action) => action())).then((results) => {
-                
-                    let r2 = results;
-                    console.log(r, r2, refs)
 
-                    
-                    let union = unionWith(r[0], r2[0], (arrVal, othVal) => {
-                        for(var k in refs){
+                    let r2 = results;
+                    console.log([...r, ...r2])
+                    let union = unionWith(...r, ...r2, (arrVal, othVal) => {
+                        for (var k in refs) {
                             console.log("Checking ref", k, arrVal, othVal)
-                            if(isEqual(`${arrVal[k]}`, `${othVal[k]}`)){
+                            if (isEqual(`${arrVal[k]}`, `${othVal[k]}`)) {
                                 console.log("Checked", arrVal[k], othVal[k], arrVal[k] == othVal[k])
                                 return merge(othVal, arrVal)
                             }
@@ -179,13 +178,14 @@ export default class MergedAdapter extends BaseAdapter{
 
                     return union;
                 })
+
             })
-        }  
+        }
     }
 
-    updateProvider(){
+    updateProvider() {
         const { actions, supporting } = this.doActions((adapter, bucket, provides) => {
-            return adapter.updateProvider({name:bucket}, this.type, provides);
+            return adapter.updateProvider({ name: bucket }, this.type, provides);
         })
 
         return (id, update) => {
@@ -209,12 +209,12 @@ export default class MergedAdapter extends BaseAdapter{
                 })
             })
         }
-        
+
     }
 
 
     //Merged addProvider, batches requests to the appropriate adapter path and creates a cross ref
-    addProvider(){
+    addProvider() {
         const { primaryActions, supportingActions } = this.sortActions(this.type)
 
         const { refs } = this.paths;
@@ -223,36 +223,36 @@ export default class MergedAdapter extends BaseAdapter{
         let supporting = [];
 
         primaryActions.forEach((action) => {
-            for(var store in action){
-                for(var path in action[store]){
+            for (var store in action) {
+                for (var path in action[store]) {
                     let adapter = new MongoAdapter(this.storeList.getStore(store).db)
                     console.log(`=> Add Provider ${path} ${this.type} ${JSON.stringify(action[store][path])}`)
-                    let add = adapter.addProvider({name: path}, this.type, action[store][path])
+                    let add = adapter.addProvider({ name: path }, this.type, action[store][path])
                     actions.push(add);
                 }
             }
         })
-        
+
         supportingActions.forEach((action) => {
-            for(var store in action){
-                for(var path in action[store]){
+            for (var store in action) {
+                for (var path in action[store]) {
                     let adapter = new MongoAdapter(this.storeList.getStore(store).db)
                     console.log(`=> Supporting Add Provider ${path} ${this.type} ${JSON.stringify(action[store][path])}`)
 
                     let hasRef = false;
                     let _refs = {};
 
-                    for(var k in refs){
-                        let indx = refs[k].map((x) => x.split(':').slice(0,2).join(':')).indexOf(`${store}:${path}`)
-                        if(indx > -1){
+                    for (var k in refs) {
+                        let indx = refs[k].map((x) => x.split(':').slice(0, 2).join(':')).indexOf(`${store}:${path}`)
+                        if (indx > -1) {
                             _refs[refs[k][indx].split(':')[2]] = k
                         }
                     }
 
-                     merge(_refs, action[store][path])
+                    merge(_refs, action[store][path])
 
-            
-                    let add = adapter.addProvider({name: path}, this.type, _refs)
+
+                    let add = adapter.addProvider({ name: path }, this.type, _refs)
                     supporting.push(add)
                 }
             }
@@ -260,9 +260,9 @@ export default class MergedAdapter extends BaseAdapter{
 
         return (object) => {
             return Promise.all(actions.map((x) => x(object))).then((result) => {
-                let resultObj = {...object};
+                let resultObj = { ...object };
                 result.forEach(item => {
-                    for(var k in item){
+                    for (var k in item) {
                         resultObj[k] = item[k]
                     }
                 })
@@ -271,9 +271,9 @@ export default class MergedAdapter extends BaseAdapter{
                 return Promise.all(supporting.map((action) => {
                     return action(resultObj)
                 })).then((results) => {
-                    let finalResult = {...resultObj}
+                    let finalResult = { ...resultObj }
                     results.forEach(item => {
-                        for(var k in item){
+                        for (var k in item) {
                             finalResult[k] = item[k]
                         }
                     })
@@ -287,9 +287,9 @@ export default class MergedAdapter extends BaseAdapter{
 
     }
 
-    deleteProvider(){
+    deleteProvider() {
         const { actions, supporting } = this.doActions((adapter, bucket, provides) => {
-            return adapter.deleteProvider({name:bucket}, this.type, provides);
+            return adapter.deleteProvider({ name: bucket }, this.type, provides);
         })
 
         //TODO write protect external dbs by default so we dont go deleting waterfalls
@@ -303,7 +303,7 @@ export default class MergedAdapter extends BaseAdapter{
                 result.forEach(item => {
                     removedCount += item.result.n;
                 })
-                
+
                 console.log(result);
                 return Promise.all(supporting.map((sAction) => {
                     return sAction(query)
